e22431fda1022485ab6d1531407e726a4007d77c
SEOYONGENIE <syj6016@hanmail.net>
2021-05-16 18:23:28 +0900

    initial commit

---
+++ HelloWorldApp.java
@@ -0,0 +1,26 @@
+public class HelloWorldApp {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!!");
+		 
+		char jumsu='C'; 
+
+        switch(jumsu){
+        case 'F' : 
+        	System.out.print(0);
+        	break;
+        case 'D' : 
+        	System.out.print(1);
+        	break;
+        case 'C' : 
+        	System.out.print(2);
+        	break;
+        case 'B' : 
+        	System.out.print(3);
+        	break;
+        case 'A' : 
+        	System.out.print(4);
+        	break;
+        }
+	}
+}
